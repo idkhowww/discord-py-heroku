759b6c5f810c53f45ba00c49de4d7c36264fd332
idkhowww <108700460+idkhowww@users.noreply.github.com>
2022-07-04 16:21:16 -0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,6 @@
 import os
 import discord
-
+import random
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -12,6 +12,22 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 if __name__ == "__main__":
     bot.run(TOKEN)
