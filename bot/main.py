9a0db31cb3d9ad88f09b9c79e91957d9a9193e69
idkhowww <108700460+idkhowww@users.noreply.github.com>
2022-07-04 16:17:05 -0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,4 @@
 import os
-import random
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
@@ -8,7 +7,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
-
+import random
 @client.event
 async def on_message(message):
     if message.author == client.user:
